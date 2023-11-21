b3a05676234ab209623438c8064954e7f2c8d224
BenjaminGautier01 <benjamingautier7@gmail.com>
2023-11-21 09:43:03 +0000

    //@ Ben // this client instance will be used to  make calls or send request to the (HallRental Api) // the URL is the address that points to the API Component.

--- HallRentalClient/Program.cs
+++ HallRentalClient/Program.cs
@@ -5,7 +5,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//@ Ben
+// this client instance will be used to  make calls or send request to the (HallRental Api)
+// the URL is the address that points to the API Component.
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7254/") });
 
 await builder.Build().RunAsync();
