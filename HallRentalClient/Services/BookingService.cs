e79505d83eafeff986f439b92d5ad575d1cbb115
BenjaminGautier01 <benjamingautier7@gmail.com>
2023-11-22 08:00:38 +0000

    Added all the client services these classcommunicate with the dto classes and the with the help of the DB context CRUD operations will be done and fetch appropriately to their appropriate routed views.

---
+++ HallRentalClient/Services/BookingService.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using HallRentalModels.Dtos; // Assuming this is the correct namespace for your DTOs
+
+namespace HallRentalClient.Services
+{
+    public class BookingService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl = "api/bookings"; // Base URL for the bookings endpoint
+
+        public BookingService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // Fetch all bookings
+        public async Task<IEnumerable<BookingDto>> GetAllBookingsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<BookingDto>>(_baseUrl) ?? Enumerable.Empty<BookingDto>();
+        }
+
+        // Fetch a single booking by ID
+        public async Task<BookingDto?> GetBookingByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<BookingDto>($"{_baseUrl}/{id}");
+        }
+
+        // Create a new booking
+        public async Task<BookingDto?> CreateBookingAsync(BookingDto newBooking)
+        {
+            var response = await _httpClient.PostAsJsonAsync(_baseUrl, newBooking);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<BookingDto>();
+            }
+            else
+            {
+                // Handle errors or throw an exception
+                throw new HttpRequestException("Error creating booking");
+            }
+        }
+
+        // Update an existing booking
+        public async Task UpdateBookingAsync(int id, BookingDto updatedBooking)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{_baseUrl}/{id}", updatedBooking);
+            if (!response.IsSuccessStatusCode)
+            {
+                // Handle errors or throw an exception
+                throw new HttpRequestException("Error updating booking");
+            }
+        }
+
+        // Delete a booking
+        public async Task DeleteBookingAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                // Handle errors or throw an exception
+                throw new HttpRequestException("Error deleting booking");
+            }
+        }
+    }
+}
--- HallRentalClient/Services/CustomerService.cs
+++ HallRentalClient/Services/CustomerService.cs
@@ -2,22 +2,68 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Net.Http.Json;
-using HallRentalModels.Dtos;    // Making sure to include the namespace where the DTOs are
+using HallRentalModels.Dtos;    // DTO namespace
 
 namespace HallRentalClient.Services
 {
-
-
     public class CustomerService
     {
         private readonly HttpClient _httpClient;
+        private readonly string _baseUrl = "api/customers"; // Base URL for the customers endpoint
 
         public CustomerService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-       
+        // Fetch all customers
+        public async Task<IEnumerable<CustomerDto>> GetAllCustomersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<CustomerDto>>(_baseUrl) ?? Enumerable.Empty<CustomerDto>();
+        }
+
+
+        // Fetch a single customer by ID
+        public async Task<CustomerDto?> GetCustomerByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<CustomerDto>($"{_baseUrl}/{id}");
+        }
+
+        // Create a new customer
+        public async Task<CustomerDto?> CreateCustomerAsync(CustomerDto newCustomer)
+        {
+            var response = await _httpClient.PostAsJsonAsync(_baseUrl, newCustomer);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<CustomerDto>();
+            }
+            else
+            {
+                // Handle errors or throw an exception
+                throw new HttpRequestException("Error creating customer");
+            }
+        }
+
+        // Update an existing customer
+        public async Task UpdateCustomerAsync(int id, CustomerDto updatedCustomer)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{_baseUrl}/{id}", updatedCustomer);
+            if (!response.IsSuccessStatusCode)
+            {
+                // Handle errors or throw an exception
+                throw new HttpRequestException("Error updating customer");
+            }
+        }
+
+        // Delete a customer
+        public async Task DeleteCustomerAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_baseUrl}/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                // Handle errors or throw an exception
+                throw new HttpRequestException("Error deleting customer");
+            }
         }
     }
 }
